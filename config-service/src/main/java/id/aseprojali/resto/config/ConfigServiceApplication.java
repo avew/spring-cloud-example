0d22d806cceab007396ae22c396923bbbbbaf47e
Asep Rojali <aseprojali@gmail.com>
2017-07-27 16:52:31 +0700

    init

---
+++ config-service/src/main/java/id/aseprojali/resto/config/ConfigServiceApplication.java
@@ -0,0 +1,33 @@
+package id.aseprojali.resto.config;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.core.env.Environment;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+@SpringBootApplication
+@EnableConfigServer
+public class ConfigServiceApplication {
+
+    private static final Logger log = LoggerFactory.getLogger(ConfigServiceApplication.class);
+
+
+    public static void main(String[] args) throws UnknownHostException {
+        SpringApplication app = new SpringApplication(ConfigServiceApplication.class);
+
+
+        Environment env = app.run(args).getEnvironment();
+        log.info("Access URLs:\n----------------------------------------------------------\n\t" +
+                 "Local: \t\thttp://127.0.0.1:{}\n\t" +
+                 "External: \thttp://{}:{}\n----------------------------------------------------------",
+
+                env.getProperty("server.port"),
+                InetAddress.getLocalHost().getHostAddress(),
+                env.getProperty("server.port"));
+    }
+}
