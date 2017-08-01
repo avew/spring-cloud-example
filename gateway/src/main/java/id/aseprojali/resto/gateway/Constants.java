58335b441c60bac2d63379a0c4cb2debba218dfd
Asep Rojali <aseprojali@gmail.com>
2017-08-01 19:03:10 +0700

    Add gateway

---
+++ gateway/src/main/java/id/aseprojali/resto/gateway/Constants.java
@@ -0,0 +1,15 @@
+package id.aseprojali.resto.gateway;
+
+/**
+ * Created by avew on 8/1/17.
+ */
+public final class Constants {
+
+    private Constants() {
+    }
+
+    public static final String SPRING_PROFILE_DEVELOPMENT = "dev";
+
+    public static final String SPRING_PROFILE_PRODUCTION = "prod";
+
+}
